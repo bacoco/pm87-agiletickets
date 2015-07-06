cca453d4533ea0127e0c146acd3d846fc2ed5240
Pedro Rabello <pedrommrabello@gmail.com>
2015-07-06 15:13:17 -0300

    MaiorMenorTest variavel MaiorMenor mam extraida como local

--- src/main/java/br/com/caelum/agiletickets/maiormenor/MaiorMenorTest.java
+++ src/main/java/br/com/caelum/agiletickets/maiormenor/MaiorMenorTest.java
@@ -2,46 +2,46 @@ package br.com.caelum.agiletickets.maiormenor;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MaiorMenorTest {
 
-	@Test
-	public void numerosEmQualquerOrdem(){
-		MaiorMenor alg = new MaiorMenor();
+	private MaiorMenor alg;
+	
+	@Before
+	public void setUp() {
+		alg = new MaiorMenor();
+	}
 
-		alg.encontra(new int[] { 4, 15, 7, 8});
-		
+	@Test
+	public void numerosEmQualquerOrdem() {
+		alg.encontra(new int[] { 4, 15, 7, 8 });
+
 		assertEquals(15, alg.getMaior());
 		assertEquals(4, alg.getMenor());
 	}
-	
-	@Test
-	public void crescente(){
-		MaiorMenor alg = new MaiorMenor();
 
-		alg.encontra(new int[] {1,2,3,4,5});
-		
+	@Test
+	public void crescente() {
+		alg.encontra(new int[] { 1, 2, 3, 4, 5 });
+
 		assertEquals(5, alg.getMaior());
 		assertEquals(1, alg.getMenor());
 	}
-	
-	@Test
-	public void decrescente(){
-		MaiorMenor alg = new MaiorMenor();
 
-		alg.encontra(new int[] {5,4,3,2,1});
-		
+	@Test
+	public void decrescente() {
+		alg.encontra(new int[] { 5, 4, 3, 2, 1 });
+
 		assertEquals(5, alg.getMaior());
 		assertEquals(1, alg.getMenor());
 	}
-	
-	@Test
-	public void unicoNumero(){
-		MaiorMenor alg = new MaiorMenor();
 
-		alg.encontra(new int[] {1});
-		
+	@Test
+	public void unicoNumero() {
+		alg.encontra(new int[] { 1 });
+
 		assertEquals(1, alg.getMaior());
 		assertEquals(1, alg.getMenor());
 	}
